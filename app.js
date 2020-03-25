a36753ad49f2d6352226c8a9ed7ef09b15fcb3fc
Benjaminas Visockis <benjamvi@stud.ntnu.no>
2020-03-25 14:15:28 +0100

    Restructured environment

--- app.js
+++ app.js
@@ -1,20 +1,36 @@
 var express = require('express');
 var socket = require('socket.io');
+var ejs = require('ejs');
 
 // App setup
+const port = 80;
+
 var app = express();
-var server = app.listen(80, function(){
-    console.log('Listening for requests on port 80...');
+app.set('view engine', 'ejs');
+var server = app.listen(port, function(){
+    console.log('%s Listening for requests on port %d...', Date().toString(), port);
 });
 
 // Static files
-app.use(express.static('public'));
+app.use('/assets', express.static('assets'));
+
+//HTTP reqs
+app.get('/', (req, res) => {
+    console.log(Date().toString(), "Requested URL: ", req.url);
+    res.render('index');
+});
+
+app.get('/chat', (req, res) => {
+    console.log("Requested URL: ", req.url);
+    res.render('chat');
+});
+
 
 // Socket setup & pass server
 var io = socket(server);
 io.on('connection', (socket) => {
 
-    console.log('Made a socket connection. Socket ID:', socket.id);
+    console.log(Date().toString(), 'Made a socket connection. Socket ID:', socket.id);
 
     // Handle chat event
     socket.on('chat', function(data){
